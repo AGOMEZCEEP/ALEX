379535eb29036ef755eb488850c81680db01f531
usuario <usuario@LAPTOP-EBET0HUE>
2020-05-21 21:23:11 +0200

    963

--- ALEX/src/aaa/bbbe.java
+++ ALEX/src/aaa/bbbe.java
@@ -1,5 +1,19 @@
 package aaa;
 
 public class bbbe {
+	/**
+	 *
+	 * @author Alex Gómez
+	 * @version 1.0.1
+	 */
+	public int ingresar(int cantidad) throws Exception {
+		if (cantidad < 0)
+			throw new Exception("No se puede ingresar una cantidad negativa");
 
+		return cantidad;
+	}
+
+	/*
+	 * Metodo para retirar cantidades en la cuenta. Modifica el saldo.
+	 */
 }
